2073a0e90b1afc4a7a965c26eebb6184c3d0ef4c
Jota <jotacodigos@gmail.com>
2024-01-24 00:34:47 -0500

    NUEVO CAMBIO

--- back/controllers/Encabezado.js
+++ back/controllers/Encabezado.js
@@ -1,5 +1,6 @@
 const { Encabeza, sequelize } = require("../models/Conexion");
 const { QueryTypes } = require("sequelize");
+const nodemailer = require("nodemailer");
 
 /* const ListarEncabezados = async (req, res) => {
   try {
@@ -14,6 +15,20 @@ const ListarEncabezados = async (req, res) => {
   try {
     const Encabezados = await sequelize.query(
       "SELECT encabezado.idEncabezado, encabezado.FechayHora, encabezado.Total, encabezado.idEstado, usuario.Nombres, usuario.Apellidos, metodopago.Descripcion AS 'MetodoPago' FROM encabezado JOIN usuario ON encabezado.idUsuario = usuario.idUsuario JOIN metodopago ON encabezado.idMetodo = metodopago.idMetodo",
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
+    res.send({ id: 200, mensaje: Encabezados });
+  } catch (error) {
+    res.send({ id: 400, mensaje: error.message });
+  }
+};
+
+const ListarEncabezadosUser = async (req, res) => {
+  try {
+    const Encabezados = await sequelize.query(
+      "SELECT encabezado.idEncabezado, encabezado.FechayHora, encabezado.Total, encabezado.idEstado, usuario.Nombres, usuario.Apellidos, metodopago.Descripcion AS 'MetodoPago' FROM encabezado JOIN usuario ON encabezado.idUsuario = usuario.idUsuario JOIN metodopago ON encabezado.idMetodo = metodopago.idMetodo WHERE ",
       {
         type: QueryTypes.SELECT,
       }
@@ -71,9 +86,41 @@ const ListarUno = async (req, res) => {
   } catch (error) {}
 };
 
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: "jotacodigos@gmail.com",
+    pass: "hfub ayrx cvip ymrv",
+  },
+});
+const enviarCorreo = async (req, res) => {
+  // send mail with defined transport object
+
+  const info = await transporter.sendMail({
+    from: '"Prueba de Envio 👻" <jotacodigos@gmail.com>', // sender address
+    to: "juan3407rincon@gmail.com, mafevillada030417@gmail.com", // list of receivers
+    subject: "Hola Compañerita ✔ 👋🏾🫂", // Subject line
+    text: "Espero que te encuentres bien :)", // plain text body
+    html: "<h1>Un Saludito</h1><br></br><h3>Que descanse, Mañana nos vemos y le doy un abracito <3 </h3><br></br><p>Si es que Quiere </p>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  //
+  // NOTE: You can go to https://forwardemail.net/my-account/emails to see your email delivery status and preview
+  //       Or you can use the "preview-email" npm package to preview emails locally in browsers and iOS Simulator
+  //       <https://github.com/forwardemail/preview-email>
+  //
+};
+
 module.exports = {
   ListarEncabezados,
   AgregarEncabezados,
   ListarUno,
   EditarEstadoEncabezado,
+  enviarCorreo,
 };
